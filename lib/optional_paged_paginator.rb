cdaa1e5293235cfe47d4bd8676141c74bd1e663b
Josh Justice <me@codingitwrong.com>
2020-07-19 15:29:42 -0400

    Fix gem path

--- lib/optional_paged_paginator.rb
+++ lib/optional_paged_paginator.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-OptionalPagedPaginator = OptionalPaginator.for(PagedPaginator)
+OptionalPagedPaginator =
+  JSONAPI::Resources::OptionalPaginators::OptionalPaginator.for(PagedPaginator)
